cf432fe606bf369376a9feebfa728e677d8cdc44
guilletdev <guilletdev@gmail.com>
2024-10-16 16:30:25 -0300

    Agrego hook catFact

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,37 @@
-import {useState, useEffect} from 'react'
-import '../src/App.css' 
-import { getRandomFact } from './services/facts'
-import { useCatImg } from './hook/useCatImg'
+import "../src/App.css";
+import { useCatImg } from "./hook/useCatImg";
+import { useCatFact } from "./hook/useCatFact";
+
 
 /* const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact' */
 /* const CAT_ENPOINT_IMAGE_URL: `https://cataas.com/cat/says/${firstword}?fontSize=50&fontColor=red&json=true` */
 /* const URL = 'https://cataas.com/cat?json=true'
 const IMAGE_URL = 'https://cataas.com/cat/says/hello?fontSize=50&fontColor=red' */
 
-export const App = ()=>{
-    const [fact, setFact] = useState()
-    const {imageUrl} = useCatImg({fact})
-    
 
-    
-    
 
-    //para recuperar la cita al cargar la pagina.
-    useEffect(()=>{
-        
-        getRandomFact().then(newFact => setFact(newFact))
-    },[])
+export const App = () => {
+  const { fact, refreshFact } = useCatFact();
+  const { imageUrl } = useCatImg({ fact });
 
-    
+  const handleClick = async () => {
+    refreshFact();
+  };
 
-    const handleClick = async ()=>{
-        const newFact = await getRandomFact()
-        setFact(newFact)
-    }
-  
-
-    return(
-        <main>
-                <h1>App de Gatitos</h1>
-                <button onClick={handleClick}>Get new Fact</button>
-                <section>
-                {fact &&  <p>{fact}</p> }
-                {imageUrl && <img src={imageUrl} alt={`image extracted using the first three words for ${fact}`} />  }
-                </section>
-                
-        </main>
-            
-            
-            
-           
-        
-    )
-}
+  return (
+    <main>
+      <h1>App de Gatos</h1>
+      <button onClick={handleClick}>Get new Fact</button>
+      <section>
+        {fact && <p>{fact}</p>}
+        {imageUrl && (
+          <img
+            src={imageUrl}
+            alt={`image extracted using the first three words for ${fact}`}
+          />
+        )}
+      </section>
+      
+    </main>
+  );
+};
